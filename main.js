a73fe13094d626b6ec626e97902febc017fd33fc
Danny Nguyen <99750567+dannynguyeninnovation@users.noreply.github.com>
2022-05-18 13:42:32 +0000

    main

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
-  <h1>meow</h1>
+  <h1>ur mom :D</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
