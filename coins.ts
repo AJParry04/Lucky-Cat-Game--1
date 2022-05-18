8c52072a5b5e3b7b533a136b7d52658a28e5c456
Allison Parry <99750022+AJParry04@users.noreply.github.com>
2022-05-18 14:04:17 +0000

    coinstuff

---
+++ coins.ts
@@ -0,0 +1,18 @@
+import {blueCoin.png} from '.images';
+import {goldCoin.png} from 'images';
+import {greenCoin.png} from 'images';
+import {pinkCoin.png} from 'images';
+import {silverCoin.png} from 'images';
+
+export let coin = {
+  x: 64,
+  y: 64,
+ image: goldCoin.png,
+}
+
+export let blueCoin = {
+  x: 64,
+  y: 64,
+  image: blueCoin.png
+}
+
