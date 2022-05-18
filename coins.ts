820672ba337fbe935dff53c3a2f92b7d3e7ba761
Allison Parry <99750022+AJParry04@users.noreply.github.com>
2022-05-18 14:27:07 +0000

    image.ts

--- coins.ts
+++ coins.ts
@@ -1,8 +1,8 @@
-import {blueCoin.png} from '.images';
-import {goldCoin.png} from 'images';
-import {greenCoin.png} from 'images';
-import {pinkCoin.png} from 'images';
-import {silverCoin.png} from 'images';
+import {blueCoin.png} from './images';
+import {goldCoin.png} from './images';
+import {greenCoin.png} from './images';
+import {pinkCoin.png} from './images';
+import {silverCoin.png} from './images';
 
 export let coin = {
   x: 0,
@@ -34,3 +34,42 @@ export let sCoin = {
   image: silverCoin.png,
 }
 
+function drawCoin() {
+  let ctx = canvas.getContext('2d');
+  ctx.resetTransform();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.translate(coin.x + 25, coin.y + 25);
+  ctx.drawImage(
+    coin.image,
+    -55,
+    -55,
+    50,
+    50
+  )
+}
+
+function updateCoin(elapsed: number) {
+  const drift = 0;
+  const fallSpeed = -75;
+  coin.y += fallSpeed * elapsed / 1000;
+  //CHANGE THESE NUMBERS WHEN YOU KNOW THE CANVAS SIZE!!!!
+  if (coin.y < 50) {
+    coin.y = 500;
+  }
+  if (coin.x < 0) {
+    coin.x = canvas.width - 25;
+  }
+  if (coin.x < canvas.width) {
+    coin.x = 240;
+  }
+}
+
+function drawScoin() {
+  let ctx = canvas.getContext ('2d');
+  ctx.resetTransform ();
+  ctx.translate (coinx+25, coin.y+25);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(
+    sCoin.image
+  )
+}
