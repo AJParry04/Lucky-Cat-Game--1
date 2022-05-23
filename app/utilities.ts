005f482d091e0dc0e8cacc8c33615e5380cfbeb7
William Tuleja <51178809+Sirbucket@users.noreply.github.com>
2022-05-23 05:39:00 +0000

    code clarity

--- app/utilities.ts
+++ app/utilities.ts
@@ -129,25 +129,33 @@ export const objectCollisionDetection = (object1: collision, object2: collision)
 
 //key1,key2,key3,key4 = d,a,w,s , velMod is a velocity modifier, elapsed is the time elapsed since the last update (I think?) eventType is the type of keystroke event. element is what element the listener is added to.
 export const objectMovementKeyboard = (object: canvasImage, element: Element, eventType: string, key1: string, key2: string, key3: string, key4: string, velMod: number, elapsed: number) => {
+	
 	const vel: number = (velMod * elapsed) / 1000; //1000 = ms
+	
 	if (element === undefined) {
 		element = canvas;
 	}
+	
 	if (eventType === undefined) {
 		eventType = "keydown";
 	}
+	
 	if (key1 === undefined) {
 		key1 = "";
 	}
+	
 	if (key2 === undefined) {
 		key2 = "";
 	}
+	
 	if (key3 === undefined) {
 		key3 = "";
 	}
+	
 	if (key4 === undefined) {
 		key4 = "";
 	}
+	
 	element.addEventListener(eventType, (event) => {
 		if (event.key === key1) {
 			object.vy = 0;
