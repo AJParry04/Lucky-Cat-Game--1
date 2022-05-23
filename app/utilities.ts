029f230e94f9cc487f18387cadffa1597f869d12
William Tuleja <51178809+Sirbucket@users.noreply.github.com>
2022-05-23 16:32:01 +0000

    Convert to standard.

--- app/utilities.ts
+++ app/utilities.ts
@@ -7,7 +7,7 @@ import { ctx } from "./canvas";
 const clearMod: number = 2;
 
 //Draws a simple canvas image with no animations.
-export const drawCanvasImage = (object: canvasImage) => {
+export function drawCanvasImage(object: canvasImage) {
 	ctx.clearRect(
 		object.x - object.sizex / clearMod,
 		object.y - object.sizey / clearMod,
@@ -25,7 +25,7 @@ export const drawCanvasImage = (object: canvasImage) => {
 };
 
 //Draws a sprite sheet with inputs for the panning width and height.
-export const drawCanvasSprite = (object: canvasImage, panw: number, panh: number) => {
+export function drawCanvasSprite(object: canvasImage, panw: number, panh: number) {
 	ctx.clearRect(
 		object.x - object.sizex / clearMod,
 		object.y - object.sizey / clearMod,
@@ -51,7 +51,7 @@ Handles world collisions for an object.
 left top right and bottom should be 0, 0, canvas.width, canvas.height.
 Specifically used to only flip one objects colliding switch when colliding with the world.
 */
-export const worldCollisionDetection = (object: collision, left: number, top: number, right: number, bottom: number) => {
+export function worldCollisionDetection(object: collision, left: number, top: number, right: number, bottom: number) {
 	if (object.bottomCollision < bottom) {
 		object.colliding = false;
 	}
@@ -86,7 +86,7 @@ export const worldCollisionDetection = (object: collision, left: number, top: nu
 };
 
 //Hopefully working object collision!
-export const objectCollisionDetection = (object1: collision, object2: collision) => {
+export function objectCollisionDetection(object1: collision, object2: collision) {
 	if (object1.leftCollision < object2.rightCollision) (object1.topCollision > object2.bottomCollision); {
 		object1.colliding = false;
 		object2.colliding = false;
@@ -111,6 +111,7 @@ export const objectCollisionDetection = (object1: collision, object2: collision)
 		object1.colliding = true;
 		object2.colliding = true;
 	}
+	
 	if (object1.rightCollision <= object2.leftCollision) (object1.topCollision <= object2.bottomCollision); {
 		object1.colliding = true;
 		object2.colliding = true;
@@ -128,7 +129,7 @@ export const objectCollisionDetection = (object1: collision, object2: collision)
 };
 
 //key1,key2,key3,key4 = d,a,w,s , velMod is a velocity modifier, elapsed is the time elapsed since the last update (I think?) eventType is the type of keystroke event. element is what element the listener is added to. Intended for up down left and right movement but COULD be used for horizontal or vertical only by labeling them with "".
-export const objectMovementKeyboard = (object: canvasImage, element: Element, key1: string, key2: string, key3: string, key4: string, velMod: number, elapsed: number) => {
+export function objectMovementKeyboard(object: canvasImage, element: Element, key1: string, key2: string, key3: string, key4: string, velMod: number, elapsed: number) {
 
 	const vel: number = (velMod * elapsed) / 1000; //1000 = ms
 
@@ -160,7 +161,7 @@ export const objectMovementKeyboard = (object: canvasImage, element: Element, ke
 };
 
 //Scrolls through the sprite reel horizontally. Vertical support may be added if we make assets with vertical panning but unlikely. Functions similarly to movement keyboard with the exception of there only being 2 buttons allowed and they must be used. Probably should be bound to what movement keys we're currently working with.
-export const objectSpriteReelKeyboard = (object: canvasImage, element: Element, key1: string, key2: string, reelMod: number, reelMaxWidth: number) => {
+export function objectSpriteReelKeyboard(object: canvasImage, element: Element, key1: string, key2: string, reelMod: number, reelMaxWidth: number) {
 	let reel: number = 0;
 	element.addEventListener("keydown", (event) => {
 		if (reel < reelMaxWidth) {
