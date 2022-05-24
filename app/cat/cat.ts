edeb4cb31baf2d2062542d174f512fedc0aa4717
JD415 <99751867+JoshDeJesus@users.noreply.github.com>
2022-05-24 16:19:57 +0000

    Added the start if animation code and began keyboard

--- app/cat/cat.ts
+++ app/cat/cat.ts
@@ -1,8 +1,32 @@
 import {drawCanvasImage, drawCanvasSprite} from '../utilities'
 import {imageMap} from '../images'
 import {objectMap} from '../globals'
+import {ctx} from '../canvas'
 
 imageMap.catImage.addEventListener("load", () => {
 	drawCanvasSprite(objectMap.cat, 64, 0);
 })
 
+
+/*
+let startTime = null
+
+function animate(timestamp: number = 0) {
+  let elapsed;
+  if (timestamp) {
+    if (!startTime) {
+      startTime = timestamp;
+      elapsed = 0;
+    } else {
+      elapsed = timestamp - startTime
+      startTime = timestamp;
+    }
+    //Move(elapsed); //plug in move code here
+  }
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawCanvasSprite();
+  requestAnimationFrame(animate)
+}
+
+requestAnimationFrame(animate)
+*/
