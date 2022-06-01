c0675f7c32083d3409912998e27bb15bca0009dd
Danny Nguyen <99750567+dannynguyeninnovation@users.noreply.github.com>
2022-06-01 14:22:24 +0000

    cute moving background done by danny with help from angie ;)

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 import "./app/";
 import "./style.css";
+
+// select the item we want -- this assumes we have
+// <div id="sprite">
+let div = document.querySelector('#background'); 
+
+let frame = 0; // current frame
+let nframes = 2000; // number of frames
+let size = 64; // size of each frame
+
+function showNextFrame () {
+  frame = (frame + 1) % nframes; // add one, loop back to zero
+  let offset = frame * size;
+  div.style.backgroundPosition = `-${offset}px`;
+}
+
+setInterval( // run forever on a timer
+  showNextFrame,
+  2500 / 12 // 12 times per second
+)
