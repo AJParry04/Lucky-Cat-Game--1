862773d594c55f3186afe9cb9e31282d52bbe7f2
Allison Parry <99750022+AJParry04@users.noreply.github.com>
2022-06-03 16:01:26 +0000

    randomizer coin

--- app/coins/coin.ts
+++ app/coins/coin.ts
@@ -3,14 +3,15 @@ import {drawCanvasImageNoClear} from "../utilities"
 
 
 objectMap.goldCoin.x = 500;
-objectMap.goldCoin.y = 500;
+objectMap.goldCoin.y = 700 + Math.random ()*1400-700;
 
 export function drawGoldCoin(elasped: number) {
     drawCanvasImageNoClear(objectMap.goldCoin);
 }
 
 export function moveCoin (elapsed:number) {
-      objectMap.goldCoin.vx = -50;
+      objectMap.goldCoin.vx = -200;
      console.log(goldCoin.vx);
   goldCoin.x += (goldCoin.vx * elapsed / 1000)
 }
+
